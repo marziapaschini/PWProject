7013c35d06636a0c58960babc59e15a8718f6a7c
marziapaschini <marzia.paschini@gmail.com>
2023-02-13 16:19:37 +0100

    Useless comments removed

--- app/db.js
+++ app/db.js
@@ -2,13 +2,12 @@ const { MongoClient } = require("mongodb");
 const url = "mongodb://mongosrv";
 const client = new MongoClient(url);
 
-let _db; // specie di campo privato
+let _db;
 
-// esportiamo un oggetto con 2 metodi
 module.exports = {
   connect: async () => {
     await client.connect();
     _db = client.db("MessaglyDB");
   },
-  getDb: () => _db, // sarebbe {return _db}
+  getDb: () => _db,
 };
